5f0c3620fb24f3c7827dd58bb586fe529f3927d8
Aditya Thakar <69141779+adityathakar8@users.noreply.github.com>
2021-08-16 17:46:19 +0530

    with ejs

--- weather.js
+++ weather.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const https = require("https");
 const parser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
+app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(parser.urlencoded({
   extended: true
 }));
 app.get("/", function(req, res) {
 
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 
 });
 
@@ -20,16 +22,16 @@ app.post("/", function(req, res) {
     console.log(response.statusCode);
     response.on("data", function(data) {
       const mydata = JSON.parse(data);
-      const weather = mydata.main.temp;
+      const weather = mydata.main.temp+" °C";
       const weatherDescription = mydata.weather[0].description;
       const icon = mydata.weather[0].icon;
       const imgUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.render("post",{weather_title: weather, weather_des: weatherDescription, weather_img: imgUrl})
 
-
-      res.write("<h1 style='background-color:#DDDDDD; text-align:center; font-size:500%;'>The temperature in " + cityName + " is " + weather + " degree celcius.</h1>");
-      res.write("<h2 style='background-color:#DDDDDD; text-align:center; font-size:300%;'>The weather is currently '" + weatherDescription + "'.</h2>");
-      res.write("<img style='widht:450px; height:450px; position:absolute; left: 35%; top:50%;' src=" + imgUrl + "></img>");
-      res.send();
+      // res.write("<h1 style='background-color:#DDDDDD; text-align:center; font-size:500%;'>The temperature in " + cityName + " is " + weather + " degree celcius.</h1>");
+      // res.write("<h2 style='background-color:#DDDDDD; text-align:center; font-size:300%;'>The weather is currently '" + weatherDescription + "'.</h2>");
+      // res.write("<img style='widht:450px; height:450px; position:absolute; left: 35%; top:50%;' src=" + imgUrl + "></img>");
+      // res.send();
 
     });
   });
